cbe5342a25429e106ad25109b2ed2fe53e3ab144
austintynes <austintynes10@gmail.com>
2023-10-13 14:24:11 -0500

    Question Array Completed

--- index.js
+++ index.js
@@ -7,9 +7,87 @@ const questions = [
 {
     type: 'input', 
     name: 'title',
-    message: 'What is the title of your Project?'
-}
+    message: 'What is the title of your Project?:',
+},
+{
+    type: 'input', 
+    name: 'description',
+    message: 'Provide a description of your project: ',
+},
+{
+    type: 'input', 
+    name: 'motivation',
+    message: 'What was your motivation for building the project?: ',
+},
+{
+    type: 'input', 
+    name: 'installation',
+    message: 'Provide usage information: ',
+},
+{
+    type: 'input', 
+    name: 'credits',
+    message: 'List of collaberators if any: ',
+},
+{
+    type: 'list', 
+    name: 'license',
+    message: 'Choose a license for your project: ',
+    choices: ['MIT', 'Apache', 'GPL', 'None'],
+},
+{
+    type: 'input', 
+    name: 'contributing',
+    message: 'Provide contribution guidelines: ',
+},
+{
+    type: 'input', 
+    name: 'tests',
+    message: 'Provide test instructions: ',
+},
+{
+    type: 'input', 
+    name: 'github',
+    message: 'What is your GitHub username?: ',
+},
+{
+    type: 'input', 
+    name: 'email',
+    message: 'What is your email address?: ',
+},
+];
 
-]
+// Generate README content
+
+function generateREADME(answers) {
+
+return `
+
+# ${answers.title}
+
+## Description
+# ${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Credits](#installation)
+- [Installation](#installation)
+- [Installation](#installation)
+- [Installation](#installation)
+- [Installation](#installation)
+- [Installation](#installation)
+
+
+
+
+
+
+
+
+`
+
+
+
+}
 
   
