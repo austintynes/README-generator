265c9b61fdd6df3b87b247d83af1c0613d7fe48b
austintynes <austintynes10@gmail.com>
2023-10-13 14:08:30 -0500

    first question in question array

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// Brought over read me guidelines
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Array of questions for user input
+const questions = [
+
+{
+    type: 'input', 
+    name: 'title',
+    message: 'What is the title of your Project?'
+}
+
+]
 
   
